8b2cbf68eaff2465803a3b5bf2e6a0b2b5288001
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-27 22:14:22 -0400

    Programación de Tareas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmailVerificationReminderCommand;
+use App\Console\Commands\SendNewsletterCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SendEmailVerificationReminderCommand::class,
+        SendNewsletterCommand::class
     ];
 
     /**
@@ -24,7 +27,21 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')
+            ->evenInMaintenanceMode()
+            ->sendOutputTo(storage_path('inspire.log'))
+            ->everyMinute();
+        $schedule->call(function (){
+            echo "hola";
+        })->everyFiveMinutes();
+        $schedule->command(SendNewsletterCommand::class)
+            ->withoutOverlapping()
+            ->onOneServer()
+            ->daily();
+        $schedule->command(SendEmailVerificationReminderCommand::class)
+            ->onOneServer()
+            ->daily();
+
     }
 
     /**
