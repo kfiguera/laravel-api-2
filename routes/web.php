f5e1daf4da31a04cf48814b1c046d1c4c61acc49
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-29 14:44:48 -0400

    Uso de Queues y Jobs

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Auth::routes(['verify' => true]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
